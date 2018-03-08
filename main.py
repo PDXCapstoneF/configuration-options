3dd3700e2fea8526fe1dff09fcf74e7c87cf08b0
bspriggs <ben@sprico.com>
2018-03-07 17:47:06 -0800

    add examples, do some sample parsing

--- main.py
+++ main.py
@@ -8,9 +8,33 @@ Options:
     --version   Show version
 
 """
-from strictyaml import load, Map, Str, Int, Seq, YAMLError
+
 from docopt import docopt
+
+from strictyaml import load, Map, Str, Int, Seq, YAMLError
+from hjson import loads as hjsonLoads
+
+schema = Map({
+        "name": Str(),
+        "age": Int(),
+})
+
+def loadYaml(possiblyYaml):
+    return load(possiblyYaml, schema).data
+
+
+loaders = {
+    '.hjson': hjsonLoads,
+    '.yml': loadYaml,
+    '.yaml': loadYaml,
+}
 
 if __name__ == "__main__":
     arguments = docopt(__doc__, version='0.0.1')
     print(arguments)
+    with open(arguments['<file>']) as f:
+        l = loaders.get(arguments['--type'], lambda x: "blah")
+        try:
+            print(l(f.read()))
+        except Exception as e:
+            print(e)
